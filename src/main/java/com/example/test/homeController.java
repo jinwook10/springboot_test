7962714672bbc1b5f25e62dda4aa804ac2be0df7
jinwook <koemtntkim97@gmail.com>
2022-07-19 11:46:51 +0900

    스프링부트 폴더이동

---
+++ src/main/java/com/example/test/homeController.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+	
+	@RequestMapping(value = "/index2")
+	public String index2() {
+		return "index2";
+	}
+}
