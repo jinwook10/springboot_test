7c98b36c9127165627599b360f2dbbec33eb2e4d
jinwook <koemtntkim97@gmail.com>
2022-07-22 18:24:47 +0900

    시큐리티 시도중

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //지우면 기존 시큐리티 비활성화
+        //super.configure(http);
+    }
+}
