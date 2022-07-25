214b5ff4579efb01694a412959637c4cbc274b13
jinwook <koemtntkim97@gmail.com>
2022-07-25 10:48:03 +0900

    시큐리티 기본세팅

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,17 +1,20 @@
 package com.example.config;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@EnableWebSecurity  //SecurityConfig로 시큐리티 제어
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //지우면 기존 시큐리티 비활성화
-        //super.configure(http);
+        super.configure(http);
     }
 }
